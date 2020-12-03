e20d1a8dc819c7c4e830a0724123d653ad4952de
wengrs <flower_be_forever@live.com>
2020-12-04 00:28:58 +0800

    12/04/2020 00:28:58

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+pub mod vector;
+pub mod color;
+extern crate image;
+
 fn main() {
-    println!("Hello, world!");
+    println!("P3\n255");
+    println!("256 256");
+    for i in 0..256 {
+        for j in 0..256 {
+            let r = (((i as f64) / 256.1)*256.) as u32;
+            let g = (((j as f64) / 256.1)*256.) as u32;
+            let b = 1;
+            println!("{0} {1} {2}", r, g, b);
+        }
+    }
 }
